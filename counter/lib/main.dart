ce9c1aec4b7412118a47626ae7f2cc3a8c85c3fe
priyalpatel2709 <piyu9552@gmail.com>
2023-07-12 09:29:36 +0530

    working...

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() =>runApp(MyFristApp() );
-  
-  
+void main() => runApp(MyFirstApp());
 
-class MyFristApp extends StatelessWidget {
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return  MaterialApp(
-      title: "my frist app",
-      home: Material(
-        color: const Color.fromARGB(255, 92, 174, 241),
-        child: Center(
-          child: Text(
-            "hello from priyal",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(color: Colors.white,fontSize: 40.0),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My First App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Material(
+          color: Color.fromARGB(255, 92, 174, 241),
+          child: Center(
+            child: Text(
+              "Hello from Priyal",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 40.0),
+            ),
           ),
         ),
       ),
     );
-    
   }
-
-
 }
