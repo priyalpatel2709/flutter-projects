3344c48759a63f5f3af5150beb91c16426e2ea19
priyalpatel2709 <piyu9552@gmail.com>
2023-07-12 18:14:22 +0530

    yoo them working :)

--- counter/lib/app_screens/them_demo.dart
+++ counter/lib/app_screens/them_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../ui_helper/utli.dart';
 
 class ThemDemo extends StatelessWidget {
 const ThemDemo({ Key? key }) : super(key: key);
@@ -9,29 +10,29 @@ const ThemDemo({ Key? key }) : super(key: key);
       child: Column(
         children: [
           Container(
-            width: 100,
+            width: 150,
             margin: EdgeInsets.only(left: 5),
-            child: Text('Name')
+            child: Text('Name',style: myTextStyle44(),)
           ),
           // Text('Priyal',style: Theme.of(context.textTheme.headline1),),
-          Text('Priyal',),
+          Text('Priyal',style: myTextStyle20(),),
           Container(
             width: 100,
             margin: EdgeInsets.only(left: 5),
-            child: Text('City')
+            child: Text('City',style: myTextStyle44(),)
           ),
           Container(
             
-            child: Text('vadodra')
+            child: Text('vadodra',style: myTextStyle20(),)
           ),
           Container(
             width: 100,
             margin: EdgeInsets.only(left: 5),
-            child: Text('Job')
+            child: Text('Job',style: myTextStyle44(),)
           ),
           Container(
             
-            child: Text('React')
+            child: Text('React',style: myTextStyle20(),)
           ),
 
         ],
---
+++ counter/lib/ui_helper/utli.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+TextStyle myTextStyle1(){
+  return TextStyle(
+    fontSize: 11,
+    color: Colors.blueGrey
+  );
+}
+
+TextStyle myTextStyle44(){
+  return TextStyle(
+    fontSize: 44,
+    color: Colors.teal
+  );
+}
+
+TextStyle myTextStyle20(){
+  return TextStyle(
+    fontSize: 20,
+    color: Colors.teal,
+    backgroundColor: Colors.amber
+  );
+}
