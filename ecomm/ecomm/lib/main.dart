5e052d602762d66c3d07065dbacde003c526152c
priyalpatel2709 <piyu9552@gmail.com>
2023-07-19 11:37:41 +0530

    new set-up

--- ecomm/ecomm/lib/main.dart
+++ ecomm/ecomm/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -18,15 +19,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 27, 1, 1),
+          title: Text('E-comm',style: TextStyle(color: Colors.white),),
+        ),
+        body: MyHomePage(),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -36,22 +41,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text('E-comm'),
-      ),
-      body: Center(
+    return Center(
 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children:[
             const Text(
               'welcome to e_comm:',
             ),
           ],
         ),
-      ),
-    );
+      );
   }
 }
