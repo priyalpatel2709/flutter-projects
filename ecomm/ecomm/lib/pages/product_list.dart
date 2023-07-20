00d169897fecdf32ced192d6f33309333fafb771
priyalpatel2709 <piyu9552@gmail.com>
2023-07-20 14:22:11 +0530

    with nice ui

--- ecomm/ecomm/lib/pages/product_list.dart
+++ ecomm/ecomm/lib/pages/product_list.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, must_be_immutable
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -8,69 +6,78 @@ import 'package:http/http.dart' as http;
 import '../Models/productlist.dart';
 
 class ProductList extends StatelessWidget {
-  ProductList({ Key? key }) : super(key: key);
-
-  List<AllProductList> productlist = [];
+  ProductList({Key? key}) : super(key: key);
 
   Future<List<AllProductList>> getProduct() async {
-      final responce = await http.get(Uri.parse('https://srever-ecomm.vercel.app/products'));
-      var data = jsonDecode(responce.body.toString());
-      if(responce.statusCode == 200){
-        for(var i in data){
-          productlist.add(AllProductList.fromJson(i));
-        }
-        return productlist;
-      }else{
-        return productlist;
+    final response =
+        await http.get(Uri.parse('https://srever-ecomm.vercel.app/products'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      List<AllProductList> productlist = [];
+      for (var i in data) {
+        productlist.add(AllProductList.fromJson(i));
       }
-      
+      return productlist;
+    } else {
+      return [];
+    }
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return FutureBuilder(
       future: getProduct(),
-      builder: (context,snapshot){
-          if (snapshot.connectionState == ConnectionState.waiting) {
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
-        }else if (snapshot.hasError) {
+        } else if (snapshot.hasError) {
           return Center(child: Text('Error: ${snapshot.error}'));
         } else if (!snapshot.hasData) {
           return Center(child: Text('No data available'));
-        }else{
+        } else {
           return ListView.builder(
-            itemCount: snapshot.data!.length,
+            itemCount: snapshot.data!.length + 1, // +1 for header row
             itemBuilder: (context, index) {
-              var product = snapshot.data![index];
-              return SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: ListTile(
-                  title: Row(
-                    children: [
-                      Container(
-                        width: 88.25,
-                        child: Text(product.name.toString())
-                      ),
-                      Container(
-                        width: 87.25,
-                        child: Text(product.price.toString())
-                      ),
-                      Container(
-                        width: 88.25,
-                        child: Text(product.category.toString())
-                      ),
-                      Container(
-                        width: 88.25,
-                        child: Text(product.company.toString())
-                      ),
-                      ]
+              if (index == 0) {
+                // Header Row
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
                   ),
-                  )
-              ); 
-                },
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                        Expanded(child: Text('Name')),
+                        Expanded(child: Text('Price')),
+                        Expanded(child: Text('Category')),
+                        Expanded(child: Text('Company')),
+                      ],
+                    ),
+                  ),
                 );
+              } else {
+                // Data Rows
+                var product = snapshot.data![index - 1];
+                return Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: ListTile(
+                    title: Row(
+                      children: [
+                        Expanded( child: Text(product.name.toString())),
+                        Expanded(child: Text(product.price.toString())),
+                        Expanded(child: Text(product.category.toString())),
+                        Expanded(child: Text(product.company.toString())),
+                      ],
+                    ),
+                  ),
+                );
+              }
+            },
+          );
         }
-    }
+      },
     );
   }
 }
