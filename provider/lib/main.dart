d733443e85ebb4efeb69dde69c55c8ddd4abc8d1
priyalpatel2709 <piyu9552@gmail.com>
2023-07-25 12:31:32 +0530

    1st provider is done

--- provider/lib/main.dart
+++ provider/lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'pages/homepage.dart';
+import 'package:provider/provider.dart';
+import 'provider/counter_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +14,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 57, 138, 184)),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => countProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 57, 138, 184)),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
-
-
--- provider/lib/pages/homepage.dart
+++ provider/lib/pages/homepage.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_app/provider/counter_provider.dart';
+
+import '../provider/counter_provider.dart';
+import '../provider/counter_provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -10,25 +15,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  
   @override
   Widget build(BuildContext context) {
+    print('asd');
+    final CountProvider = Provider.of<countProvider>(context,listen: false);
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text('Provider'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
+        child: Consumer<countProvider>(builder: (context, value, child) {
+          return Text(value.count.toString(),style: TextStyle(fontSize: 50,fontWeight: FontWeight.w900), );
+        })
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+          CountProvider.setCount();
+      },
+      child: Icon(Icons.add),
+      elevation: 20,
       ),
     );
   }
