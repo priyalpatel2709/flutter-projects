47c0404d1c413ee3525042d080ea21ef3fcb15d2
priyalpatel2709 <piyu9552@gmail.com>
2023-09-04 18:04:49 +0530

    set-up

---
+++ aapni_vato/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'pages/splace_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Aapni Vato'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Splace_screen();
+  }
+}
