223cbf3a143d0ba01009b58dd8ac910b2d8e1d3b
priyalpatel2709 <piyu9552@gmail.com>
2023-10-09 16:53:54 +0530

    upload-working

--- img_store/lib/pages/homepage.dart
+++ img_store/lib/pages/homepage.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../utilits/uploadtocloude.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,6 +14,55 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  bool imgLoading = false;
+  final picker = ImagePicker();
+  File? selectedImage;
+  bool isImg = false;
+  var picUrl = '';
+
+  Future<void> pickAndUploadImage() async {
+    imgLoading = true;
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        selectedImage = File(pickedFile.path);
+      });
+
+      final imageUrl = await uploadImageToCloudinary(selectedImage!);
+
+      if (imageUrl != null) {
+        imgLoading = false;
+        isImg = true;
+        picUrl = imageUrl;
+        // _controller.text = picUrl;
+        setState(() {});
+      } else {
+        imgLoading = false;
+        // showDialog(
+        //   context: context,
+        //   builder: (BuildContext context) {
+        //     return ErrorDialog(
+        //       title: 'Fail',
+        //       message: 'Failed to upload image to Cloudinary ',
+        //     );
+        //   },
+        // );
+      }
+    } else {
+      imgLoading = false;
+      // showDialog(
+      //   context: context,
+      //   builder: (BuildContext context) {
+      //     return ErrorDialog(
+      //       title: 'Fail',
+      //       message: 'No image selected',
+      //     );
+      //   },
+      // );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,20 +74,16 @@ class _HomepageState extends State<Homepage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'App',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            !imgLoading
+                ? const Text(
+                    'You have pushed the button this many times:',
+                  )
+                : CircularProgressIndicator(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
-        },
+        onPressed: pickAndUploadImage,
         child: const Icon(Icons.cloud),
       ),
     );
