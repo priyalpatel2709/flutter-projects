20075925c383b929541fee9c66f4bbb64c8c0f2f
priyalpatel2709 <piyu9552@gmail.com>
2023-10-09 16:24:46 +0530

    setUp done

---
+++ img_store/android/app/src/main/kotlin/com/example/img_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.img_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ img_store/lib/pages/homepage.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Gallery'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'App',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+        },
+        child: const Icon(Icons.cloud),
+      ),
+    );
+  }
+}
