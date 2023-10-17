f5e8f8300f6a09e8ee6b30289b80566919239db6
Priyal2709 <146949345+Priyal2709@users.noreply.github.com>
2023-10-17 14:14:03 +0530

    without Hive

--- shopping_app/lib/pages/callinfg.dart
+++ shopping_app/lib/pages/callinfg.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-import '../data/selectedstudentdata.dart';
 
 class CallScreen extends StatefulWidget {
   @override
@@ -12,15 +9,13 @@ class CallScreen extends StatefulWidget {
 class _CallScreenState extends State<CallScreen> {
   final List phoneNumbers = ['+1234567890', '+918141513344'];
   // SelectedStudent db = SelectedStudent();
-  List <dynamic> selectedStudend = [];
+  List<dynamic> selectedStudend = [];
   late dynamic count;
   bool isLastCall = false;
   @override
   void initState() {
     super.initState();
     // selectedStudend = db.loadData();
-    getData();
-    
     count = 0;
   }
 
@@ -89,10 +84,5 @@ class _CallScreenState extends State<CallScreen> {
         ),
       ),
     );
-  }
-  
-  Future<void> getData() async {
-    final crpoBox = await Hive.openBox('crpoBox');
-    selectedStudend = crpoBox.get('crpoKey', defaultValue: []);
   }
 }
